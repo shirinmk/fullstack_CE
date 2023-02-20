c26439573536427daf2d8738dd47885f79a14451
shery <sh_manoochehri@yahoo.com>
2023-02-20 13:48:12 -0800

    initial file

---
+++ Assignment2/js/01-larger-or-smaller.js
@@ -0,0 +1,8 @@
+// Larger or Smaller?
+
+// Usage: Use simple conditional statements
+
+// Create an application that accepts two integers within two separate prompts. Then, display only the larger of the two within the browser window. Don’t forget to handle the fact that the two could be equal.
+
+
+
---
+++ Assignment2/js/02-word-translator.js
@@ -0,0 +1,7 @@
+// The Word Translator
+
+//  Usage: Use simple conditional statements
+
+// Create an application that prompts the user for a language code ("es", "de", "en", and "fr"). If they enter one of the four codes, display the translated version of "Hello World" for the given language within the console window. If the user enters anything but those four language codes, the application should default to English. The result should be (for example):
+
+// Hello World translated in French is: Bonjour le monde
