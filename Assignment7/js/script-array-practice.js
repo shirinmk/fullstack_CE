f1dc6271e5bb9cee3fbc993daf373cfb8b96d66b
shery <sh_manoochehri@yahoo.com>
2023-04-12 13:49:39 -0700

    need to check another time

--- Assignment7/js/script-array-practice.js
+++ Assignment7/js/script-array-practice.js
@@ -32,7 +32,6 @@
 
 //STEP 17
 
-
 // M8 Assignment - Part 1, Practice with Arrays (20 points)
 
 //*************STEP1**************************** */
@@ -91,7 +90,6 @@
 // for(let movie in movies){
 //     console.log(movies[movie]) //ATTENTIN MOVIE IS INDEX
 // }
-
 
 //****************STEP6************************* */
 // Copy the code from step 5. Now, use a for/of loop to iterate through the array and display each movie within the console window.
@@ -157,7 +155,6 @@
 //     console.log(`${i}`);
 // }
 
-
 //****************STEP9************************* */
 // Copy the code from step 8. Now, use the concat() method to merge the two arrays together into a single array called movies. Use the console window to display the list in reverse sorted
 // let movies = [];
@@ -174,9 +171,6 @@
 // for(let i of movies){
 //     console.log(`${i}`);
 // }
-
-
-
 
 //****************STEP10************************* */
 // Copy the code from step 9. Use an array function to return just the last item in the array and display it within the console window.
@@ -196,7 +190,6 @@
 // }
 // let elm = movies.pop();
 // console.log(`the last element removed is ${elm}`)
-
 
 //****************STEP11************************* */
 // Copy the code from step 10. Remove the previous method and this time use a method to return just the first item in the array and display it within the console window.
@@ -228,7 +221,6 @@
 // let indexNotLike = []
 // // ??????????
 
-
 //****************STEP13************************* */
 // Create a multi-dimensional array that contains your 5 favorite movies and their ranking from 1-5. The array should look something like this:
 
@@ -237,18 +229,41 @@
 // Now, loop through the array and filter out and display only the movie names. You must use the filter() method and you’ll need to filter out the names by their primitive data type.
 
 // let movies = [
-//     ["Movie 1", 1],
-//     ["Movie 2", 2],
-//     ["Movie 3", 3],
-//     ["Movie 4", 4],
-//     ["Movie 5", 5]
-//     ];
-//     let names = movies.filter((item) => {
-//         if (typeof(${item[0]} === 'string'))
-//     }
+//   ["Movie 1", 1],
+//   ["Movie 2", 2],
+//   ["Movie 3", 3],
+//   ["Movie 4", 4],
+//   ["Movie 5", 6],
+// ];
+// let names = [];
 
-//     )
-//    ???????????????????????
+// //ONE SOLUTION
+//     for (let i = 0; i < movies.length; i++){
+//         console.log(`${movies[i][0]} : ${movies[i][1]}`)
+//         if(typeof movies[i][0] === 'string' ){
+//             names.push(movies[i][0])
+//         }
+//         if(typeof movies[i][1] === 'string' ){
+//             names.push(movies[i][1])
+//         }
+
+//     }
+//     console.log(names.forEach((name) => {
+//         console.log(name)
+//     }))
+
+// //SECOND SOLUTION
+// movies.forEach((item) => {
+//   console.log(`each element in movies is ${item}`);
+//   item.filter((itemIn) => {
+//     console.log(`each item in combine ${itemIn}`);
+//     if (typeof itemIn === "string") {
+//       names.push(itemIn);
+//     }
+//   }); //end filter
+// }); //end forEach
+// console.log(names);
+
 
 //****************STEP14************************* */
 // Create a string array called employees using literal notation and populate the array with several employee names. Then, create an anonymous function called showEmployee. The function should accept a parameter. Call this function, passing in the employees array into the function as a parameter. Make sure to display the result in the console window. Within the function, loop through the passed in array and display the result so that it looks exactly like this in the console window:
@@ -261,6 +276,17 @@
 // FRED
 // SALLY
 
+// let employees = ['ZAK','JESSICA', 'MARK', 'FRED','SALLY']
+
+// function showEmployee(...args) {
+//     console.log(`Employees:\n\n`)
+// 	for (let arg of args) {
+// 		console.log(arg)
+// 	}
+	
+// }
+// showEmployee(employees)
+	
 
 //****************STEP15************************* */
 // Write a JavaScript function to filter false, null, 0 and blank values from an array.
@@ -268,16 +294,49 @@
 // Test Data: console.log(filterValues([58, '', 'abcd', true, null, false, 0]))
 // Expected Result: [58, "abcd", true]
 
+// let items =[]
+// function  extractValues(...args) {
+//     for (let arg of args) {
+//         // console.log(arg);
+//         if((arg !== '') && (arg !== false)  && (arg !== null)  && (arg !== 0)){
+//             // console.log(arg); 
+//             items.push( arg)
+//         }//END IF
+    
+//   }
+//   return items
+// }
+  
+//   extractValues(58, '', 'abcd', true, null, false, 0);//WHEN PUTTING [] SHOWS INDEX BUT WHEN () IT SHOWS ONLY ITEMS
+//  console.log(items)
+
 //****************STEP16************************* */
 // Write a JavaScript function to get a random item from an array. So if I create a numeric array with 10 numbers and then pass that array into my function, the function should randomly return one of those numbers.
+
+// let numOfElement ;
+// let indexOfRandomElement;
+// function  extractValues(...args) {
+//     // console.log(args.length)
+//     numOfElement = args.length
+//     // console.log(Math.floor(Math.random()*numOfElement))
+//     indexOfRandomElement = Math.floor(Math.random()*numOfElement)
+//        console.log(` the index is ${indexOfRandomElement} : the element is ${args[indexOfRandomElement]} `)
+    
+//     }
+      
+//       extractValues(58, '', 'abcd', true, null, false, 0);//WHEN PUTTING [] SHOWS INDEX BUT WHEN () IT SHOWS ONLY ITEMS
 
 //****************STEP17************************* */
 // Write a JavaScript function to get the largest number from a numeric array.
 
+// let maxValue = 0;
+// function findLargest(...args){
+//     for(let arg of args){
+//         if (arg > maxValue){
+//             maxValue = arg
+//         }
+//     }
+//     return maxValue
 
-
-
-
-
-
-
+// }
+// console.log(findLargest(3,5,33,95,7,8))
