3369d6ede9298e274d1edc1f2f25ea6415dc77ab
shery <sh_manoochehri@yahoo.com>
2023-05-09 12:35:56 -0700

    initial assignment M11

---
+++ M11-assignemnt/M11-assignment/js/strings.js
@@ -0,0 +1,46 @@
+//STEP 1
+// let fname = prompt("what is your name?")
+// let lengthName = fname.length
+// alert(`Your name ${fname} has ${lengthName} letters`)
+
+//STEP 2
+// let fname = prompt("what is your name?")
+// let lengthName = fname.length
+// alert(`Your name ${fname} has ${lengthName} letters`)
+// let charNum = prompt("what number you want to know the letter?")
+// let ch = fname.charAt(parseInt(charNum))
+// alert(`${charNum} is ${ch} in ${fname} `)
+
+//STEP 3
+// let fName = prompt("What is your first name? ")
+// let lName = prompt("What is your last name? ")
+// alert(`Your name is: ${fName.concat(lName)}`)
+
+//STEP 4
+// let sentense = "The quick brown fox jumps over the lazy dog"
+// alert(`The index of fox is ${sentense.indexOf('fox')}`)
+
+//STEP 5
+// let sentense = "The quick brown fox jumps over the lazy fox"
+// alert(`The last index of fox is ${sentense.lastIndexOf('fox')}`)
+
+//STEP 6
+// let sentense = "The quick brown fox jumped over the lazy dog"
+// let fullName = prompt("What is your full name? ")
+// alert(sentense.replace('the lazy dog', fullName))
+
+//STEP 7
+// let sentense = "The quick brown fox jumps over the lazy dog"
+// let userWord = prompt('Enter the word you are looking for? ')
+// // alert(sentense.search(userWord))
+// if(sentense.search(userWord) !== -1){
+//     alert(`the position of ${userWord} is ${sentense.search(userWord)} in this sentence "${sentense}"`)
+// }else{
+//     alert(`it doesn't have ${userWord} in this sentence "${sentense}"`)
+// }
+
+//STEP 8
+
+//STEP 9
+
+//STEP 10
